75ecd50181ba9ffa28e6ce3e8619a2984fc7e9a3
hyeonsok <hyeonsok@student.42seoul.kr>
2020-12-30 18:29:01 +0900

    Fix ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,13 +6,13 @@
 /*   By: hyeonsok <hyeonsok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/29 19:03:38 by hyeonsok          #+#    #+#             */
-/*   Updated: 2020/12/30 17:00:23 by hyeonsok         ###   ########.fr       */
+/*   Updated: 2020/12/30 18:27:37 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int		isinset(char ch, char const *set);
+// static	int		isinset(char ch, char const *set);
 
 char 			*ft_strtrim(char const *s1, char const *set)
 {
@@ -23,11 +23,11 @@ char 			*ft_strtrim(char const *s1, char const *set)
 		return (0);
 	if (set == NULL || *set == '\0')
 		return (ft_strdup(s1));
-	while (isinset(*s1, set) != 0)
+	while (*s1 != '\0' && ft_strchr(set, *s1) != NULL)
 		s1++;
 	idx = ft_strlen(s1);
 	s1 += idx - 1;
-	while (idx && isinset(*s1, set))		//예외케이스 추가시켰음 .
+	while (idx != 0 && *s1 != '\0' && ft_strchr(set, *s1) != NULL)		//이게 지금 애매한 것 같다 ..  idx = 1일 때 어떻게 작동하는가 ? 
 	{
 		s1--;
 		idx--;
@@ -37,16 +37,15 @@ char 			*ft_strtrim(char const *s1, char const *set)
 	s1 -= (idx - 1);
 	return ((char *)ft_memcpy(ret, s1, idx));
 }
-
-static int		isinset(char ch, char const *set) //'\0'은 set에 해당되지 않음 
-{
-	while (set && *set)
-	{
-		if (ch == *set++)
-			return (1);
-	}
-	return (0);
-}
+// static int		isinset(char ch, char const *set) //'\0'은 set에 해당되지 않음 
+// {
+// 	while (set && *set)
+// 	{
+// 		if (ch == *set++)
+// 			return (1);
+// 	}
+// 	return (0);
+// }
 
 // #include <string.h>
 // #include <stdio.h>
