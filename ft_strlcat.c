2291bf97cc576cee569620268a24711112122d35
hyeonsok <hyeonsok@student.42seoul.kr>
2021-01-04 19:59:33 +0900

    Fix ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hyeonsok <hyeonsok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 13:26:28 by SSONG             #+#    #+#             */
-/*   Updated: 2021/01/03 20:31:15 by hyeonsok         ###   ########.fr       */
+/*   Updated: 2021/01/04 19:58:11 by hyeonsok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,10 @@ size_t		ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t dst_len;
 	size_t src_len;
 
-	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
+	if (dst == NULL && dstsize == 0)
+		return (src_len);
+	dst_len = ft_strlen(dst);
 	if (dst_len >= dstsize)
 		return (dstsize + src_len);
 	while (*dst && dst++)
